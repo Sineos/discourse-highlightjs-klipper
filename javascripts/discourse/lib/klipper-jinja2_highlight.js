4183ea856b3fb449c7c1ab933beb6a791fea9ca5
Sineos <Sineos.pub@gmail.com>
2025-03-22 17:49:30 +0100

    Update klipper-jinja2_highlight.js

--- javascripts/discourse/lib/klipper-jinja2_highlight.js
+++ javascripts/discourse/lib/klipper-jinja2_highlight.js
@@ -5,6 +5,8 @@ const COMMENT = {
   variants: [
     { begin: /^\s*#/, end: /$/, relevance: 0 }, // Standalone comments
     { begin: /#(?=\s|\w)/, end: /$/, relevance: 0 }, // Inline comments
+    { begin: /(?<=\})\s*#/, end: /$/, relevance: 0 }, // After Jinja2 blocks
+    { begin: /(?<=,)\s*#/, end: /$/, relevance: 0 }, // After comma-separated lists
   ],
 };
 
